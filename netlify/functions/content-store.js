322f8eceb3a97b4f4f3dcd7322ebd41dd6c26d9b
berimbauzinho <bernamon510@gmail.com>
2026-08-11 10:09:13 -0300

    fix: normalize team references in content storage

--- netlify/functions/content-store.js
+++ netlify/functions/content-store.js
@@ -1,5 +1,6 @@
 const { connectLambda, getStore } = require("@netlify/blobs");
 const { applyPgl2026Imports } = require("./pgl-2026-imports");
+const { normalizeContentTeamReferences } = require("./team-relations");
 
 const STORE_NAME = "hltpc-content";
 const CONTENT_KEY = "current";
@@ -11,11 +12,11 @@ function store(event) {
 
 async function getContent(event) {
   const content = await store(event).get(CONTENT_KEY, { type: "json" }) || {};
-  return applyPgl2026Imports(content);
+  return normalizeContentTeamReferences(applyPgl2026Imports(content));
 }
 
 async function saveContent(event, content) {
-  await store(event).setJSON(CONTENT_KEY, content);
+  await store(event).setJSON(CONTENT_KEY, normalizeContentTeamReferences(content));
 }
 
 module.exports = { getContent, saveContent };
