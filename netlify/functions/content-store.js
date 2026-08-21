5aea781326fdcd7b5da0cdd95215673d00efbc3a
berimbauzinho <bernamon510@gmail.com>
2026-08-20 21:35:13 -0300

    Corrige demos e progressão automática do campeonato

--- netlify/functions/content-store.js
+++ netlify/functions/content-store.js
@@ -2,6 +2,7 @@ const { connectLambda, getStore } = require("@netlify/blobs");
 const { applyPgl2026Imports } = require("./pgl-2026-imports");
 const { applyStatisticsImports } = require("./statistics-imports");
 const { normalizeContentTeamReferences } = require("./team-relations");
+const { applyTournamentProgression } = require("./tournament-progression");
 
 const STORE_NAME = "hltpc-content";
 const CONTENT_KEY = "current";
@@ -13,11 +14,11 @@ function store(event) {
 
 async function getContent(event) {
   const content = await store(event).get(CONTENT_KEY, { type: "json" }) || {};
-  return normalizeContentTeamReferences(applyStatisticsImports(applyPgl2026Imports(content)));
+  return applyTournamentProgression(normalizeContentTeamReferences(applyStatisticsImports(applyPgl2026Imports(content))));
 }
 
 async function saveContent(event, content) {
-  await store(event).setJSON(CONTENT_KEY, normalizeContentTeamReferences(content));
+  await store(event).setJSON(CONTENT_KEY, applyTournamentProgression(normalizeContentTeamReferences(content)));
 }
 
 module.exports = { getContent, saveContent };
